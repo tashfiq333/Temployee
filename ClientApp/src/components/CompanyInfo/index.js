19381e4ce865d92ac20a51bbb4468bcbe8fcbc9f
tashfiq333 <tashfiqnahiyan333@gmail.com>
2021-09-12 22:24:00 +0600

    Navbar fix korsi

--- ClientApp/src/components/CompanyInfo/index.js
+++ ClientApp/src/components/CompanyInfo/index.js
@@ -5,26 +5,42 @@ import "./companyinfo.css";
 
 const CompanyInfo = () => {
   return (
-    <div>
-      <div className="title">
-        <h2> Catagory</h2>
-        <div className="divider-custom">
-          <div className="divider-custom-line"></div>
+    <section className="comp">
+      <div>
+        <div className="title">
+          <h2> Catagory</h2>
+          <div className="divider-custom">
+            <div className="divider-custom-line"></div>
+          </div>
         </div>
-      </div>
-      <div className="row body_padding">
-        {/* <LinearProgress
+        <div className="row body_padding">
+          {/* <LinearProgress
       className="progress_bar"
       variant="determinate"
       value={50}
     /> */}
-        <CompanyCard />
-        <CompanyCard />
-        <CompanyCard />
-        <CompanyCard />
-        <CompanyCard />
+
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+          <CompanyCard />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- ClientApp/src/components/carousel/index.js
+++ ClientApp/src/components/carousel/index.js
@@ -1,55 +1,52 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import sa from '../../images/slidera.png';
-import sb from '../../images/sliderb.png';
-import sc from '../../images/sliderc.png';
-import sd from '../../images/sliderd.png';
+import sa from "../../images/slidera.png";
+import sb from "../../images/sliderb.png";
+import sc from "../../images/sliderc.png";
+import sd from "../../images/sliderd.png";
 
-//import "~slick-carousel/slick/slick.css"; 
-//import "~slick-carousel/slick/slick-theme.css";
+// import "~slick-carousel/slick/slick.css";
+// import "~slick-carousel/slick/slick-theme.css";
 
-import './carousel.css';
+import "./carousel.css";
 
 const Carousel = () => {
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+  };
 
-    const settings = {
-        className: "center",
-        centerMode: true,
-        infinite: true,
-        centerPadding: "60px",
-        slidesToShow: 3,
-        speed: 500
-      };
-
-    return(
-        <>
-
-<div>
+  return (
+    <>
+      <div>
         <h2>Featured Catagories of Skills</h2>
         <Slider {...settings}>
           <div>
             <img className="sliderimg" src={sa} alt="" />
           </div>
           <div>
-          <img className="sliderimg" src={sb} alt="" />
+            <img className="sliderimg" src={sb} alt="" />
           </div>
           <div>
-          <img className="sliderimg" src={sc} alt="" />
+            <img className="sliderimg" src={sc} alt="" />
           </div>
           <div>
-          <img className="sliderimg" src={sd} alt="" />
+            <img className="sliderimg" src={sd} alt="" />
           </div>
           <div>
-          <img className="sliderimg" src={sa} alt="" />
+            <img className="sliderimg" src={sa} alt="" />
           </div>
           <div>
-          <img className="sliderimg" src={sc} alt="" />
+            <img className="sliderimg" src={sc} alt="" />
           </div>
         </Slider>
       </div>
+    </>
+  );
+};
 
-        </>
-    );
-}
-
-export default Carousel
+export default Carousel;
