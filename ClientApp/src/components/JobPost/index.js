7ec18a8cb4f44ac6621483203040a727801aec86
tashfiq333 <tashfiqnahiyan333@gmail.com>
2021-09-20 00:04:36 +0600

    bug fix

--- ClientApp/src/components/JobPost/index.js
+++ ClientApp/src/components/JobPost/index.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import { JobcardStyle, textField } from "./jobStyles";
-import Typography from "@mui/material/Typography";
+
 import Button from "@material-ui/core/Button";
-import TextField from "@mui/material/TextField";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Chip from "@material-ui/core/Chip";
-import TextareaAutosize from "@mui/material/TextareaAutosize";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import { POST } from "../../api";
 
