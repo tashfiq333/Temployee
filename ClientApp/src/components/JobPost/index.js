db0b88a8bd8c9b2dd10b7c8afb036d6fa735c7ea
Sowad13 <mejbahurrahman13@gmail.com>
2021-09-20 01:20:39 +0600

    signer

--- ClientApp/src/components/JobPost/index.js
+++ ClientApp/src/components/JobPost/index.js
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 
-import Card from "@material-ui/core//Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import { JobcardStyle, textField } from "./jobStyles";
 import Typography from "@material-ui/core/Typography";
-
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Chip from "@material-ui/core/Chip";
